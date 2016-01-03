36c63085db754b0e07ffe8d3c9b0beedd69fe696
LilTsubaki <omenforce.clem@neuf.fr>
2016-01-04 00:06:03 +0100

    push

---
+++ Assets/Scripts/CharAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CharAnimation : MonoBehaviour
+{
+    public Animation anim;
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+        anim.Play("Walk");
+    }
+    void Update()
+    {
+        //Debug.Log(anim.gameObject.name);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.PlayQueued("Attack", QueueMode.PlayNow);
+            anim.PlayQueued("Walk", QueueMode.CompleteOthers);
+        }    
+    }
+}
